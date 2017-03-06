7944e7ace76a895a0ad645dacf72b141d95ce358
Dave Arter <davea@mysociety.org>
2017-03-06 16:26:14 +0000

    [CuidoMiCiudad] Any user with from_body set can access admin

--- perllib/FixMyStreet/Cobrand/CuidoMiCiudad.pm
+++ perllib/FixMyStreet/Cobrand/CuidoMiCiudad.pm
@@ -11,4 +11,9 @@ sub country {
 sub languages { [ 'es-do,Spanish,es_DO' ] }
 sub language_override { 'es-do' }
 
+sub admin_allow_user {
+    my ( $self, $user ) = @_;
+    return 1 if $user->is_superuser || $user->from_body;
+}
+
 1;
