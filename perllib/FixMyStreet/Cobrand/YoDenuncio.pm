4ffcf0b5c98ee6542a1190e41bf146004a53d8ec
Zarino Zappia <mail@zarino.co.uk>
2017-06-21 12:23:36 +0100

    [Yo Denuncio] Initial new cobrand.
    Don’t send questionnaires.
    Skip /around page from front form.

---
+++ perllib/FixMyStreet/Cobrand/YoDenuncio.pm
@@ -0,0 +1,23 @@
+package FixMyStreet::Cobrand::YoDenuncio;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+
+sub country {
+    return 'DO';
+}
+
+sub languages { [ 'es-do,Spanish,es_DO' ] }
+sub language_override { 'es-do' }
+
+sub admin_allow_user {
+    my ( $self, $user ) = @_;
+    return 1 if $user->is_superuser || $user->from_body;
+}
+
+sub skip_around_page { 1 }
+
+sub send_questionnaires { 0 }
+
+1;
