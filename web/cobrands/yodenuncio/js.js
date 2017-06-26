7fc73ca91d381f06281cdb28848252dcf0fd957c
Zarino Zappia <mail@zarino.co.uk>
2017-06-26 15:35:49 +0100

    Privacy banner

--- web/cobrands/yodenuncio/js.js
+++ web/cobrands/yodenuncio/js.js
@@ -15,4 +15,20 @@ $(function(){
 
         $fieldsets.eq(1).hide();
     });
+
+    var $privacyAssurance = $('.js-privacy-assurance')
+    if ( $privacyAssurance.length ) {
+        if( $.cookie('has_dismissed_privacy_assurance') ){
+            $privacyAssurance.remove();
+        } else {
+            var $close = $('<button class="privacy-assurance__close">');
+            $close.text('Cerrar').on('click', function(){
+                $.cookie('has_dismissed_privacy_assurance', 1, {expires: 30, path: '/'});
+                $privacyAssurance.slideUp(250, function(){
+                    $privacyAssurance.remove();
+                });
+            });
+            $close.appendTo( $privacyAssurance.children('.container') );
+        }
+    }
 });
