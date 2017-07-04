a7e74e34ecb349154cafa82d1b1ba4c7de4866fc
Zarino Zappia <mail@zarino.co.uk>
2017-07-04 09:35:20 +0100

    [Yo Denuncio] Geolocation link on homepage.

--- web/cobrands/yodenuncio/js.js
+++ web/cobrands/yodenuncio/js.js
@@ -1,3 +1,6 @@
+// We do our own #geolocate_link setup in dom ready.
+delete fixmystreet.set_up.report_geolocation;
+
 $(function(){
     $('.js-two-stage-frontpage-form').each(function(){
         var $form = $(this);
@@ -14,5 +17,23 @@ $(function(){
         });
 
         $fieldsets.eq(1).hide();
+    });
+
+    $('.js-geolocation-option').each(function(){
+        if (!('geolocation' in navigator)) {
+            $('#geolocate_link').hide();
+            return;
+        }
+
+        var $form = $(this).parent();
+
+        fixmystreet.geolocate.setup(function(pos) {
+            var params = {
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                category: $form.find('[name="category"]:checked').val()
+            };
+            location.href = '/report/new?' + $.param(params);
+        });
     });
 });
